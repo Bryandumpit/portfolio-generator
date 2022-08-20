b100ebb6c3da870e99d420964df0fb0575d9351c
Bryan Dumpit <dumpitbr@gmail.com>
2022-08-20 18:25:40 -0400

    generate HTML using node fileWrite

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template')
+
 const profileDataArgs = process.argv.slice(2,process.argv.length);
-console.log(profileDataArgs);
 
+const [name, github]=profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i+=1){
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('======================================')
-
-    profileDataArr.forEach(profileItem=>
-        console.log(profileItem))
-}
-
-
-
-
-//this is the same as
-// const printProfileData = function (profileDataArr){ console.log(profileDataArr)};
-//if only one parameter, can omit parenthesis e.g. const printProfileData = profileDataArr => {console.log(profileDataArr)};
-//if only one action, can omit curly braces e.g. const printProfileData = profileDataArr => console.log(profileDataArr);
-//const addNums = (numONe, numTwo) => numOne + numTwo;
-//const sum = addNums(5,3);
-
-printProfileData(profileDataArgs);
-
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
